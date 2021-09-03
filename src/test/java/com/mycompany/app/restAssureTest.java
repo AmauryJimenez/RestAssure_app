07321c0034d793ac66cbc2897a2dd8b5bb9448f7
AmauryJ <AmauryJ@QA-AMAURYJ>
2021-09-02 21:24:19 -0500

    watherTest

--- src/test/java/com/mycompany/app/restAssureTest.java
+++ src/test/java/com/mycompany/app/restAssureTest.java
@@ -1,51 +1,73 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertArrayEquals;
+//import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import io.restassured.RestAssured;
-import io.restassured.http.Method;
+//import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.path.json.JsonPath;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class restAssureTest 
-{
+public class restAssureTest {
+
     @Test
-        public void BmiInputs18_5to25ShouldBeNormal()
-        {
-            RestAssured.baseURI = "https://restapi.demoqa.com/utilities/weather/city";
-            // RequestSpecification httpRequest = RestAssured.given();
-            // Response response = httpRequest.get("/Hyderabad");
-        
-            // // First get the JsonPath object instance from the Response interface
-            // JsonPath jsonPathEvaluator = response.jsonPath();
-        
-            // // Then simply query the JsonPath object to get a String value of the node
-            // // specified by JsonPath: City (Note: You should not put $. in the Java code)
-            // String city = jsonPathEvaluator.get("City");
-        
-            // // Let us print the city variable to see what we got
-            // System.out.println("City received from Response " + city);
-        
-            // // Validate the response
-            // assertEquals(city,"Hyderabad");
-            // //Assert.assertEquals(city, "Hyderabad", "Correct city name received in the Response");
-        }
-    
-        // @Tests
-        // public void InvalidBmiInputsShouldreturnvalidationerror()
-        // {
-        //     given().
-        //         queryParam("weight", "0").
-        //         queryParam("height", "0").
-        //     when().
-        //         get("http://sampleapi.azurewebsites.net/api/bmi").
-        //     then().    
-        //         statusCode(400);
-        // }
+    public void swapiTest() {
+        RestAssured.baseURI = "https://swapi.dev/api/films";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/1");
+        System.out.println("content type " + response.then().extract().contentType());
+        System.out.println("status code " + response.then().extract().statusCode());
+        // First get the JsonPath object instance from the Response interface
+        JsonPath jsonPathEvaluator = response.jsonPath();
+
+        List<String> charactersList = jsonPathEvaluator.get("characters");
+        assertEquals(charactersList.size(), 18);
+
+        String title = jsonPathEvaluator.get("title");
+        int episode_id = jsonPathEvaluator.get("episode_id");
+        String opening_crawl = jsonPathEvaluator.get("opening_crawl");
+        String director = jsonPathEvaluator.get("director");
+        String producer = jsonPathEvaluator.get("producer");
+        String release_date = jsonPathEvaluator.get("release_date");
+
+        // Let us print the city variable to see what we got
+        System.out.println("response: " + "\n" + title + "\n" + episode_id + "\n" + opening_crawl + "\n" + director
+                + "\n" + producer + "\n" + release_date);
+
+        String jsonstring = response.asString();
+        // Validate the response
+        assertEquals(jsonstring.contains("A New Hope"), true);
+        // Assert.assertEquals(city, "Hyderabad", "Correct city name received in the
+        // Response");
     }
+
+    @Test
+    public void watherTest() {
+
+        RestAssured.baseURI = "https://bookstore.toolsqa.com/";
+        RequestSpecification request = RestAssured.given();
+        Response response = request.get("BookStore/v1/Books");
+   
+
+        System.out.println(response.getStatusCode()); 
+        
+        String jsonstring = response.asString();
+        JsonPath jsonPathEvaluator = response.jsonPath();
+
+        List<String> books = jsonPathEvaluator.get("books");
+        
+
+        System.out.println(books); 
+
+        assertEquals(jsonstring.contains("Learning JavaScript Design Patterns"), true);
+
+
+
+    }
+}
